bafe03d88165625eb5adef5e12a8ff474e8e7718
Leonardo Ascione <me@leonardfactory.com>
2014-03-24 20:14:47 +0100

    Added API to create new product

--- route/general.js
+++ route/general.js
@@ -25,6 +25,43 @@ var app 	= module.exports = express();
 		], next);
 	});
 	
+	app.post('/product', function (req, res, next)
+	{
+		var _preview_id;
+		
+		async.waterfall([
+			function (callback) 
+			{
+				Product.create({
+					name 		: req.body.name,
+					description	: req.body.description,
+					price		: req.body.price,
+					discountedPrice : req.body.discountedPrice,
+					shop		: mongoose.Types.ObjectId(req.body.shop)
+				}, callback);
+			},
+			function (product, callback) 
+			{
+				Preview.create({
+					title	   : req.body.name,
+					showcase   : req.body.showcase,
+					price	   : req.body.price,
+					discountedPrice : req.body.discountedPrice
+				}, callback);
+			},
+			function (preview, callback) 
+			{
+				_preview_id = preview._id;
+				
+				Shop.where({ _id : req.body.shop }).update({ $push : { showcase : preview }}).exec(callback);
+			},
+			function (callback) 
+			{
+				res.json(200, { added: true, preview_id: _preview_id});
+			}
+		], next);
+	});
+	
 	app.get('/hello', function (req, res, next)
 	{
 		res.json(200, { done: 'done' });
