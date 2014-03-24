fe603981360dda44b58ef0eb3e75985477a9b0b6
Leonardo Ascione <me@leonardfactory.com>
2014-03-24 12:45:29 +0100

    Fixed package.json

---
+++ route/general.js
@@ -0,0 +1,21 @@
+var express 	= require('express'),
+	config		= require('../config/config'),
+	async 		= require('async'),
+	erroneous	= require('../services/erroneous');
+
+var Product		= require('../model/product').model;
+
+var app 	= module.exports = express();
+
+(function (app) 
+{
+	app.get('/products', function (req, res, next) 
+	{
+		async.waterfall([
+			function (callback) 
+			{
+				callback(null);
+			}
+		]);
+	});
+})
